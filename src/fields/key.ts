9871868c824112ea8dc48d7ab92a8bdc99ad5bc3
Adam Baker <bakerac4@gmail.com>
2022-01-19 10:00:10 -0500

    Add key

---
+++ src/fields/key.ts
@@ -0,0 +1,32 @@
+import { Orbit } from '@orbit/core';
+import { KeyDefinition } from '@orbit/records';
+
+import Model from '../model';
+import { defineKey } from '../utils/model-definition';
+
+const { assert } = Orbit;
+
+export interface TrackedKey {
+    get(this: Model): string;
+    set(this: Model, value: string): void;
+}
+
+export default function key(options: KeyDefinition = {}): any {
+    return (target: Model, property: string): TrackedKey => {
+        function get(this: Model): string {
+            return this.$getKey(property) ?? '';
+        }
+
+        function set(this: Model, value: string) {
+            const oldValue = this.$getKey(property);
+
+            if (value !== oldValue) {
+                this.$replaceKey(property, value);
+            }
+        }
+
+        defineKey(target, property, options as KeyDefinition);
+
+        return { get, set };
+    };
+}
